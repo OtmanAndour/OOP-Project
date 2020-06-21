225e87f5cab6d321444cccd4cb50982c8ff6dd5a
Othman Andour <andour.otman@gmail.com>
2020-06-21 23:34:56 +0200

    Learning about instance variables and class variables

--- employee.py
+++ employee.py
@@ -3,23 +3,28 @@
 
 class Employee:
 
+    num_of_emps = 0
+    raise_amount = 1.04
+
     def __init__(self, first, last, pay):
         self.first = first
         self.last = last
         self.pay = pay
         self.email = first + "." + last + "@company.com"
 
+        Employee.num_of_emps += 1
+
     def full_name(self):
         return '{} {}'.format(self.first, self.last)
+
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amount)
+
+
+print(Employee.num_of_emps)
 
 
 emp_1 = Employee("Otman", "Andour", 50000)
 emp_2 = Employee("Test", "User", 60000)
 
-# print(emp_1)
-# print(emp_2)
-
-print(emp_1.full_name())
-print(emp_2.full_name())
-
-print(Employee.full_name(emp_1))
+print(Employee.num_of_emps)
